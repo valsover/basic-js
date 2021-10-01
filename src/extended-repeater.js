cc059a9dfe2f8415902e7a1aa699966217944e30
valsover <68599608+valsover@users.noreply.github.com>
2021-10-01 16:36:43 +0300

    feat: solve task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  	if (str === null)
+		str = String(str);
+	if (options.addition === null)
+		options.addition = String(options.addition);
+	if (options.separator === undefined) {
+		options.separator = '+';
+	}
+	if (options.additionSeparator === undefined) {
+		options.additionSeparator = '|';
+  }
+	if (options.addition === undefined){
+		options.addition = '';
+	}
+	if (options.repeatTimes === undefined)
+		options.repeatTimes = 1;
+	if (options.additionRepeatTimes == undefined)
+		options.additionRepeatTimes = options.addition ? 1 : 0;
+	return new Array(options.repeatTimes).fill(str + Array(options.additionRepeatTimes).fill(options.addition).join(options.additionSeparator)).join(options.separator);
 }
